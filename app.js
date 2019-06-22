692088344037abb304349d37e56f8d4407742f20
takattty <r9r_celtics_t9t@icloud.com>
2019-06-22 13:04:25 +0900

    readFile

--- app.js
+++ app.js
@@ -1,9 +1,16 @@
 const http = require('http');
+const fs = require('fs');
 
 var server = http.createServer(
     (request, response) => {
-        response.end('Hello Node.js!!');
-    } 
+        fs.readFile('./index.html', 'UTF-8', 
+        (err, date) => {
+            response.writeHead(200, {'Content-Type': 'text/html'});
+            response.write(date);
+            response.end();
+        });
+    }
 );
 
 server.listen(3000);
+console.log('Server start!');
