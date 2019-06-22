638586a19f9d76d6c7b9b834710b7c29498763f5
takattty <r9r_celtics_t9t@icloud.com>
2019-06-22 17:49:22 +0900

    change-code

--- app.js
+++ app.js
@@ -6,14 +6,13 @@ let server = http.createServer(getFromClient);
 server.listen(3000);
 console.log('Server start!');
 
-function getFromClient(req, res) {
-  request = req;
-  response = res;
-  fs.readFile('./index.html', 'UTF-8', writeToResponse);
+function getFromClient(request, response) {
+  fs.readFile('./index.html', 'UTF-8', 
+    (error, data) => {
+      var content = data.replace(/dummy_title/g, 'これはタイトルです').replace(/dummy_content/g, 'これがコンテンツです');
+        response.writeHead(200, { 'Content-Type': 'text/html' });
+        response.write(content);
+        response.end();
+    } 
+  );
 };
-
-function writeToResponse(error, data) {
-  response.writeHead(200, { 'Content-Type': 'text/html' });
-  response.write(data);
-  response.end();
-}
