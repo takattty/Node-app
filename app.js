7967f4a14a574ce630fa96c5960271f4d8606c0c
takattty <r9r_celtics_t9t@icloud.com>
2019-06-22 17:20:49 +0900

    change-code

--- app.js
+++ app.js
@@ -9,10 +9,11 @@ console.log('Server start!');
 function getFromClient(req, res) {
   request = req;
   response = res;
-    fs.readFile('./index.html', 'UTF-8',
-      (err, date) => {
-        response.writeHead(200, { 'Content-Type': 'text/html' });
-        response.write(date);
-        response.end();
-      });
+  fs.readFile('./index.html', 'UTF-8', writeToResponse);
 };
+
+function writeToResponse(error, data) {
+  response.writeHead(200, { 'Content-Type': 'text/html' });
+  response.write(data);
+  response.end();
+}
