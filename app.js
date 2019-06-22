28e0aa4b49541cce5ae47b55c30fcfd8d3cf3297
takattty <r9r_celtics_t9t@icloud.com>
2019-06-22 17:16:42 +0900

    change-code

--- app.js
+++ app.js
@@ -1,16 +1,18 @@
 const fs = require('fs');
 const http = require('http');
 
-let server = http.createServer(
-  (request, response) => {
+let server = http.createServer(getFromClient);
+
+server.listen(3000);
+console.log('Server start!');
+
+function getFromClient(req, res) {
+  request = req;
+  response = res;
     fs.readFile('./index.html', 'UTF-8',
       (err, date) => {
         response.writeHead(200, { 'Content-Type': 'text/html' });
         response.write(date);
         response.end();
       });
-  }
-);
-
-server.listen(3000);
-console.log('Server start!');
+};
